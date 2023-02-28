4aee6818ce4e11eff2a67328b8dd33ae12c31e4b
LeoSallu <L.sallustio99@gmail.com>
2023-02-28 15:37:49 +0100

    creazione costanti e impostazione interpolazione

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 'use strict';
-
+//Array
 const list = ['pasta','pomodoro','pecorino','guanciale','pepe','peperoncino'];
+//Variabili
 let listIndex = list.length;
 let i = 0;
+let itemList = '';
+const  ulItem = document.querySelector('.list'); 
 
+//Ciclo While
 while (i < listIndex - 1){
     i++;
-    console.log(list[i]);
+    itemList += `<li>${list[i]}</li>`
+    console.log(itemList);
     i < listIndex;
 }
