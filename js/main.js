5a27af477136df211835e59e8a3bad3c5ab97148
LeoSallu <L.sallustio99@gmail.com>
2023-02-28 15:28:13 +0100

    ciclo while di prova

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const list = ['pasta','pomodoro','pecorino','guanciale','pepe','peperoncino'];
+let listIndex = list.length;
+let i = 0;
+
+while (i < listIndex - 1){
+    i++;
+    console.log(list[i]);
+    i < listIndex;
+}
